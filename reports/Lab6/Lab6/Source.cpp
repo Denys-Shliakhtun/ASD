34532f13e8539c279e1fa5e6be9f4b754ec6bff0
Denys-Shliakhtun <denys.shlakhtun@gmail.com>
2021-11-22 19:02:40 +0200

    Lab6

---
+++ reports/Lab6/Lab6/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+float sum(float num, float diff, int n)
+{
+	return n > 1 ? num + sum(num + diff, diff, --n) : num;
+}
+
+int main() {
+	float num, diff; int n;
+	cout << "Enter the first number: "; cin >> num;	
+	cout << "Enter the difference: "; cin >> diff;	
+	cout << "Enter the number of members: "; cin >> n;		
+	float result = sum(num, diff, n);
+	cout << "Result: " << result << endl;
+	system("pause");
+}
+
